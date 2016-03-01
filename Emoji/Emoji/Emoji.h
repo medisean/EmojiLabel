1bf69066c79c801cf5493e814e5f43e7891c15a5
zouliangming <liangmingzou@163.com>
2016-03-01 15:38:39 +0800

    Ini emoji label.

---
+++ Emoji/Emoji/Emoji.h
@@ -0,0 +1,19 @@
+//
+//  Emoji.h
+//  Emoji
+//
+//  Created by ZouLiangming on 16/3/1.
+//  Copyright © 2016年 ZouLiangming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Emoji.
+FOUNDATION_EXPORT double EmojiVersionNumber;
+
+//! Project version string for Emoji.
+FOUNDATION_EXPORT const unsigned char EmojiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Emoji/PublicHeader.h>
+
+
