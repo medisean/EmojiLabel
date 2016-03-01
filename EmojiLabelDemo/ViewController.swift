99db61c6ac51e2aa4a4feb896f701105bad13390
zouliangming <liangmingzou@163.com>
2016-03-01 15:44:02 +0800

    Add emoji label project.

---
+++ EmojiLabelDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmojiLabelDemo
+//
+//  Created by ZouLiangming on 16/3/1.
+//  Copyright © 2016年 ZouLiangming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
